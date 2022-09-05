2210b477da8ace41ee41852a4efaa4f27ea4fc4a
Enfili <jaroslav.pavlicko@gmail.com>
2022-09-05 17:25:12 +0200

    Region vaccinations entity class, JPA and REST services.

--- src/main/java/com/telekom/datacorona/server/DataCoronaServer.java
+++ src/main/java/com/telekom/datacorona/server/DataCoronaServer.java
@@ -8,6 +8,9 @@ import com.telekom.datacorona.hospital.HospitalService;
 import com.telekom.datacorona.hospital.HospitalServiceJPA;
 import com.telekom.datacorona.region.RegionService;
 import com.telekom.datacorona.region.RegionServiceJPA;
+import com.telekom.datacorona.regionVaccinations.RegionVaccinations;
+import com.telekom.datacorona.regionVaccinations.RegionVaccinationsJPA;
+import com.telekom.datacorona.regionVaccinations.RegionVaccinationsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -19,7 +22,8 @@ import org.springframework.context.annotation.ComponentScan;
         "com.telekom.datacorona.city",
         "com.telekom.datacorona.district",
         "com.telekom.datacorona.hospital",
-        "com.telekom.datacorona.region"
+        "com.telekom.datacorona.region",
+        "com.telekom.datacorona.regionVaccinations"
 })
 @ComponentScan(basePackages = {
         "com.telekom.datacorona.city",
@@ -51,5 +55,10 @@ public class DataCoronaServer {
     @Bean
     public RegionService regionService() {
         return new RegionServiceJPA();
+    }
+
+    @Bean
+    public RegionVaccinationsService regionVaccinationsService() {
+        return new RegionVaccinationsJPA();
     }
 }
