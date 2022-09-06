99bf5cdb6ef96bf5abeeed3e1b8e84f519241e8b
Rado / DESKTOP <dugas.radoslav@gmail.com>
2022-09-06 11:09:40 +0200

    added connection to API

--- src/main/java/com/telekom/datacorona/server/Console.java
+++ src/main/java/com/telekom/datacorona/server/Console.java
@@ -1,14 +1,43 @@
 package com.telekom.datacorona.server;
 
-import com.telekom.datacorona.region.Region;
 import com.telekom.datacorona.region.RegionService;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Console {
 
+    @Autowired
     private RegionService regionService;
 
     public void run() {
-        System.err.println("App started");
-        regionService.addRegion(new Region("Test title", "Test Code", "Test abbreviation"));
+        System.out.println("App started");
+        try {
+            URL url = new URL("https://data.korona.gov.sk/api/regions");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            System.err.println("Response code: " + responseCode);
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+                String inline = "";
+                Scanner scanner = new Scanner(url.openStream());
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+                scanner.close();
+                JSONParser parser = new JSONParser();
+                JSONObject dataObj = (JSONObject) parser.parse(inline);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
