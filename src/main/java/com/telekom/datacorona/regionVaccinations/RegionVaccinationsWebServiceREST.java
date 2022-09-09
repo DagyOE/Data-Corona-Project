afe8fe962132f41d0dc5d31adeaf36c96b5ec999
Rado / DESKTOP <dugas.radoslav@gmail.com>
2022-09-09 15:58:23 +0200

    added filter to regionvaccinationsjpa

--- src/main/java/com/telekom/datacorona/regionVaccinations/RegionVaccinationsWebServiceREST.java
+++ src/main/java/com/telekom/datacorona/regionVaccinations/RegionVaccinationsWebServiceREST.java
@@ -1,11 +1,9 @@
 package com.telekom.datacorona.regionVaccinations;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,5 +16,10 @@ public class RegionVaccinationsWebServiceREST {
     @GetMapping
     public List<RegionVaccinations> getAllRegionVaccinations() {
         return regionVaccinationsService.getAllRegionVaccinations();
+    }
+
+    @GetMapping("/{from}/{to}")
+    public List<RegionVaccinations> getCountRegionVaccinations(@PathVariable String from,@PathVariable String to) {
+        return regionVaccinationsService.getCountRegionVaccinations(from, to);
     }
 }
